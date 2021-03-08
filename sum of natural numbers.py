8d23ff05da58eb7f58d9bc7175578b5866ea85c3
vishwajeet vijay jagtap <vishwajeet300@gmail.com>
2021-03-08 23:42:50 +0530

    Create sum of natural numbers.py
    a program to print sum first 10 natural numbers in python using for loop

---
+++ sum of natural numbers.py
@@ -0,0 +1,9 @@
+#write a program to print sum first 10 natural numbers in python using for loop
+
+number = 10
+
+add=0
+for num in range(1, number + 1):
+    add+=num
+
+print("The Addition of Natural Numbers from 1 to",number,"is",add) 
